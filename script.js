4bb6b0004748b41de40eca20df477d937a607d5a
mayoneko <ev60622@meiji.ac.jp>
2020-04-09 23:39:26 +0900

    feat: add button done and delete

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function addTodo() {
     const todoList = document.getElementById("todoList");
     const todoNode = document.createElement("div");
     if (newTodoText.trim()) {
-        todoNode.innerText = newTodoText;
+        todoNode.innerHTML
+            = '<div class="todoBox">'
+            + '<p>' + newTodoText + '</p>'
+            + '<button onclick="doneTodo();">done</button>'
+            + '<button onclick="deleteTodo();">delete</button>'
+            + '</div>';
         todoList.append(todoNode);
     }
 }
